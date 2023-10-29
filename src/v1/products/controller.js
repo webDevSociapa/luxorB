4426d10d03874d998975297ad8275bdb1a126b3d
shashi kant <skntjee@gmail.com>
2023-10-29 20:22:52 +0530

    listing and prd added

--- src/v1/products/controller.js
+++ src/v1/products/controller.js
@@ -1,5 +1,5 @@
 import { categoryProductModel } from '../../models/common.js'
-import { MainCatProductModel , Products, markerCateroyModel, penCategoryModel } from './../../models/products.js'
+import { MainCatProductModel , Makers, Products, markerCateroyModel, penCategoryModel } from './../../models/products.js'
 
 
 // ---------------------addc cat -------------------
@@ -17,12 +17,6 @@ import { MainCatProductModel , Products, markerCateroyModel, penCategoryModel } 
 // }
 
 // ---------------------addc cat -------------------
-
-
-
-
-
-
 
 
 
@@ -110,22 +104,83 @@ export async function getAllProductsCategory() {
 
 
 
-
-
-
-
 // ------------get all category -------------------------
-export async function getAllCatWiseProducts(_id) {
+
+
+export async function getAllCatWiseProducts(_id , cat_type) {
      
     try{
+
+
+         let prd_names = await categoryProductModel.findById(_id)
+        //  console.log(prd_names , cat_type )
+         
+          if(prd_names==null) {
+             
+         if( cat_type.trim()=="metal pens" || cat_type.trim()=="everyday writing" ) {
+            let cat_wise_products =await Products.find({category_type:_id}).populate("category_type")       
+            return Promise.resolve(cat_wise_products) 
+
+         }else if(cat_type.trim()=="Permanent Markers" || cat_type.trim()=="WHITEBOARD MARKERS" || cat_type.trim()=="Whiteboard Care Kits"){
+            let cat_wise_products =await Makers.find({marker_category_type:_id}).populate("marker_category_type")       
+            return  Promise.resolve(cat_wise_products)
+         }
+ 
+         }else{
+            let  cat_wise_products =await MainCatProductModel.find({product_cat_type:_id}).populate("product_cat_type")       
+            return Promise.resolve(cat_wise_products)   
+         }
+
            
-       let  cat_wise_products =await MainCatProductModel.find({product_cat_type:_id}).populate("product_cat_type")
-       
-        console.log(cat_wise_products.length)
-       return Promise.resolve(cat_wise_products)
     }
     catch(err ) {
         return Promise.reject(err.message)
     }
 }
 
+
+
+export  async function getProductByOnlyId({selected_prd , prd_id}) {
+     
+try {
+    
+    let find_final_cat =""
+    let master_cat_id = prd_id
+
+    let all_prd_cat= await categoryProductModel.find({}).select('name')
+    let all_pen_cat  = await penCategoryModel.find({}).select('category')
+    let all_marker_cat  = await markerCateroyModel.find({}).select('marker_category')
+     
+     
+      const { _id: product_id , category:selected_category }  = selected_prd
+      
+       find_final_cat = all_prd_cat.find(ele=> ele._id==master_cat_id ) 
+      if(find_final_cat!=null) {
+         let main_prd = await MainCatProductModel.findById(product_id)
+         return Promise.resolve(main_prd)
+       }       
+
+       find_final_cat = all_pen_cat.find(ele=> ele._id==master_cat_id ) 
+        if(find_final_cat!=null) {
+            let pen_prd = await Products.findById(product_id)
+            return Promise.resolve(pen_prd)
+        }      
+
+       find_final_cat = all_marker_cat.find(ele=> ele._id==master_cat_id ) 
+    
+       if(find_final_cat!=null) {
+           let marker_prd = await Makers.findById(product_id)       
+            return Promise.resolve(marker_prd) 
+        }      
+
+
+       
+
+    return Promise.resolve(true)
+ } catch(err) {       
+    return Promise.reject(err.message)
+
+        }
+
+
+}
--- src/v1/products/route.js
+++ src/v1/products/route.js
@@ -1,7 +1,7 @@
 import  express  from "express";
 
 import { fail_service_response, succes_service_response } from "../../util.js";
-import {  addProduct, getAllProductsCategory, getAllCatWiseProducts  } from "./controller.js";
+import {  addProduct, getAllProductsCategory, getAllCatWiseProducts, getProductByOnlyId  } from "./controller.js";
 let prd = express.Router()
 
  
@@ -20,8 +20,9 @@ prd.get('/get-all-products-category' , (req, res )=>{
 prd.get('/get-cate-wise-products' , (req, res )=>{
 
      let cat_id  = req.query._id
+     let cat_type  = req.query.cat_type
 
-     getAllCatWiseProducts(cat_id).then((result)=>{ 
+     getAllCatWiseProducts(cat_id, cat_type).then((result)=>{ 
         res.send(succes_service_response(result)) 
      }).catch(err=>{
        res.send(fail_service_response(err)) 
@@ -39,6 +40,23 @@ prd.get('/get-cate-wise-products' , (req, res )=>{
       res.send(fail_service_response(err)) 
    })
    
+})
+
+
+
+
+
+prd.post('/get-product-by-only-id' , (req, res )=>{
+
+    let model = {}
+       model.selected_prd  =req.body.selected_prd
+       model.prd_id  =req.body.prd_id
+      getProductByOnlyId(model).then((result)=>{ 
+         res.send(succes_service_response(result)) 
+      }).catch(err=>{
+      res.send(fail_service_response(err)) 
+     })
+  
 })
 
 
