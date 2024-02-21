693b402a8f372559ba5b63fb817659acb1b4e0b6
sachin <skk4326@gmail.com>
2024-02-21 10:07:01 +0000

    deploy

---
+++ build/precache-manifest.f9dd153791018d71bffdd313f091d503.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8ef54d39eabadb41b84",
+    "url": "/static/css/main.751d47d0.chunk.css"
+  },
+  {
+    "revision": "b8ef54d39eabadb41b84",
+    "url": "/static/js/main.17cebff6.chunk.js"
+  },
+  {
+    "revision": "a9ffdbdeeb1fbb3751b4",
+    "url": "/static/js/runtime~main.955ff9f6.js"
+  },
+  {
+    "revision": "402ac0c613ad07af59ce",
+    "url": "/static/css/2.7c91f211.chunk.css"
+  },
+  {
+    "revision": "402ac0c613ad07af59ce",
+    "url": "/static/js/2.7ac568c3.chunk.js"
+  },
+  {
+    "revision": "d911408a0f9095ac7e22",
+    "url": "/static/js/3.24d99624.chunk.js"
+  },
+  {
+    "revision": "108ecf45a6e62fc205c34dc10d18e29d",
+    "url": "/index.html"
+  }
+];
